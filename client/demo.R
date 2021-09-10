7eaf7eeb99442adf06385331bcecf0bc0ff6054c
Tim Schaefer <ts+code@rcmd.org>
2021-09-10 16:40:04 +0200

    finish data loading section for IXI dataset

--- client/demo.R
+++ client/demo.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
-
+#
+# Trains various machine learning classifiers on FreeSurfer structural neuroimaging data.
+#
+# Written by Tim Schäfer, 2021-09-10
 
 library("fsbrain");
 library("kernlab");
@@ -8,39 +11,46 @@ library("readxl");
 
 #### Setup data ####
 
-study_dir = "/Volumes/shared/projects/IXI_dataset/";
-subjects_dir = file.path(study_dir, "IXI-T1/");
+get_IXI_demographics <- function(study_dir) {
+    subjects_dir = file.path(study_dir, "IXI-T1");
 
-measure = "thickness";
+    ##### Load metadata / demographics #####
 
+    ## This is a bit annoying for the IXI dataset, see comments below.
+    demographics_file = file.path(study_dir, "IXI.xls")
+    demographics = readxl::read_excel(demographics_file);
 
-##### Load metadata / demographics #####
+    ## The demographics file for the IXI dataset does NOT contain the subject data directory names
+    ## and there is no way to construct them from the data in there. We need to search the directories
+    ## in the subjects_dir for ones starting with the expected pattern and match on them. ><
+    demographics$subject_id_padded = sprintf("%03d", demographics$IXI_ID); # turn stuff like '12' into '012', like the dir names.
 
-
-## This is a bit annoying for the IXI dataset, see comments below.
-
-demographics_file = file.path(study_dir, "IXI.xls")
-demographics = readxl::read_excel(demographics_file);
-
-## The demographics file for the IXI dataset does NOT contain the subject data directory names
-## and there is no way to construct them from the data in there. We need to search the directories
-## in the subjects_dir for ones starting with the expected pattern and match on them. ><
-
-demographics$subject_id_padded = sprintf("%03d", demographics$IXI_ID); # turn stuff like '12' into '012', like the dir names.
-
-ixi_dirs_and_files = list.files(path = subjects_dir, pattern="IXI[[:digit:]]{3}-");
-ixi_dirs = ixi_dirs_and_files[file.info(file.path(subjects_dir, ixi_dirs_and_files))$isdir];
-dir_info = data.frame("subject_data_dirname"=ixi_dirs, "subject_id_padded"=substring(ixi_dirs, 4,6)); # construct field to match on.
+    ixi_dirs_and_files = list.files(path = subjects_dir, pattern="IXI[[:digit:]]{3}-");
+    ixi_dirs = ixi_dirs_and_files[file.info(file.path(subjects_dir, ixi_dirs_and_files))$isdir];
+    dir_info = data.frame("subject_data_dirname"=ixi_dirs, "subject_id_padded"=substring(ixi_dirs, 4,6)); # construct field to match on.
 
 
-metadata = base::merge(demographics, dir_info, by="subject_id_padded");     # Merge data.frames to retain only subjects with a FreeSurfer data directory.
-metadata = metadata[complete.cases(metadata),];                             # Remove all subjects with NA values in the metadata. These are subjects for which the age is unknown.
-
+    metadata = base::merge(demographics, dir_info, by="subject_id_padded");     # Merge data.frames to retain only subjects with a FreeSurfer data directory.
+    metadata = metadata[complete.cases(metadata),];                             # Remove all subjects with NA values in the metadata. These are subjects for which the age is unknown.
+    return(metadata);
+}
 
 
 ##### Load data #####
 
+measure = "thickness";
+study_dir = "/Volumes/shared/projects/IXI_dataset/";
+subjects_dir = file.path(study_dir, "IXI-T1/"); # the FreeSurfer SUBJECTS_DIR containing the neuroimaging data.
+demographics = get_IXI_demographics(study_dir);
+subjects_list = demographics$subject_data_dirname; # The directory names for the subjects, under the SUBJECTS_DIR, that are actually used for the analysis.
 data = fsbrain::group.morph.standard(subjects_dir, subjects_list, measure, fwhm = "10");
+
+
+##### Train and evaluate model #####
+
+
+
+
 
 
 
