e0dbd6e00b8bedae7bc7807d92bf95bea95f202e
Tim Schäfer <ts+code@rcmd.org>
2022-01-28 06:30:34 +0100

    use pre-imputed IQ values for ABIDE to speed-up computations

--- R/dataset_abide.R
+++ R/dataset_abide.R
@@ -4,8 +4,6 @@
 #'
 #' @param impute_data logical, whether to impute missing data with mice. The filtered data (in_DiMartino_study AND has_structural_img) contains 30 subjects with missing IQ values.
 #'
-#' @param add_merged logical, whether to add return a single merged data.frame to the return value list with key 'merged'.
-#'
 #' @param exclude_bad_quality manually excluded subjects, e.g. due to bad quality
 #'
 #' @return named list with entries 'brainstats', 'demographics' and 'subjects_list'. The first two are data.frames, the last one is a vector of character strings.
@@ -15,9 +13,8 @@
 #' @importFrom stats complete.cases
 #'
 #' @export
-load_ABIDE_metadata <- function(impute_data = TRUE, add_merged=TRUE, exclude_bad_quality=c("UM_1_0050272")) {
+load_ABIDE_metadata <- function(impute_data = TRUE, exclude_bad_quality=c("UM_1_0050272")) {
     abide_metadata = list();
-
 
     demographics_file = system.file("extdata", "ABIDE_Phenotypic_V1_0b_preprocessed1.csv", package = "brainnet", mustWork = TRUE);
     md_raw = utils::read.table(demographics_file, header=TRUE, sep = ",", na.strings = c("", "-9999"), comment.char = "");
@@ -52,26 +49,37 @@ load_ABIDE_metadata <- function(impute_data = TRUE, add_merged=TRUE, exclude_bad
     demographics$gender[md$SEX == 2] = "female";
 
     if(impute_data) {
-        demographics_and_brainstats = base::merge(demographics, brainstats, by.x="subject_id", by.y="subject");
-        #cat(sprintf("Merged demographics with %d rows and brainstats with %d, results has %d rows.\n", nrow(demographics), nrow(brainstats), nrow(demographics_and_brainstats)));
+        force_reimpute = FALSE;
+        if(force_reimpute) {
+            demographics_and_brainstats = base::merge(demographics, brainstats, by.x="subject_id", by.y="subject");
+            #cat(sprintf("Merged demographics with %d rows and brainstats with %d, results has %d rows.\n", nrow(demographics), nrow(brainstats), nrow(demographics_and_brainstats)));
 
-        #print(head(demographics_and_brainstats));
+            #print(head(demographics_and_brainstats));
 
-        ## Check for NA values in important columns: This shows that 30 subjects have an NA value for "iq".
-        #sapply(demographics_and_brainstats, function(x) sum(is.na(x)));
-        num_subjects_with_missing_data = sum(as.integer(!stats::complete.cases(demographics_and_brainstats)));
-        cat(sprintf("Imputing IQ data for %d subjects with missing data (of %d subjects total).\n", num_subjects_with_missing_data, nrow(demographics_and_brainstats)));
+            ## Check for NA values in important columns: This shows that 30 subjects have an NA value for "iq".
+            #sapply(demographics_and_brainstats, function(x) sum(is.na(x)));
+            num_subjects_with_missing_data = sum(as.integer(!stats::complete.cases(demographics_and_brainstats)));
+            cat(sprintf("Imputing IQ data for %d subjects with missing data (of %d subjects total).\n", num_subjects_with_missing_data, nrow(demographics_and_brainstats)));
 
-        ## Impute the missing iq data.
-        ##  - We only impute data in columns we are interested in, otherwise it takes ages and the results for some columns make no sense anyways.
-        ##  - We use the brainstats (computed from FreeSurfer parcellation stats using ExtractBrainMeasuresTS.bash script) to impute the values.
+            ## Impute the missing iq data.
+            ##  - We only impute data in columns we are interested in, otherwise it takes ages and the results for some columns make no sense anyways.
+            ##  - We use the brainstats (computed from FreeSurfer parcellation stats using ExtractBrainMeasuresTS.bash script) to impute the values.
 
-        #init = mice::mice(demographics_and_brainstats, maxit=0L);
-        imputation_res = mice::mice(demographics_and_brainstats, m=5, maxit=50, method='cart', seed=500, printFlag = FALSE);
-        demographics_and_brainstats_imputed = mice::complete(imputation_res);
-        ## Check again for NAs:
-        #sapply(demographics_and_brainstats_imputed, function(x) sum(is.na(x)));
-        demographics$iq = demographics_and_brainstats_imputed$iq; # actually replace the NA IQ values with the imputed ones.
+            #init = mice::mice(demographics_and_brainstats, maxit=0L);
+            imputation_res = mice::mice(demographics_and_brainstats, m=1, maxit=50, method='cart', seed=500, printFlag = FALSE);
+            demographics_and_brainstats_imputed = mice::complete(imputation_res);
+            ## Check again for NAs:
+            #sapply(demographics_and_brainstats_imputed, function(x) sum(is.na(x)));
+            demographics$iq = demographics_and_brainstats_imputed$iq; # actually replace the NA IQ values with the imputed ones.
+
+            #write.table(data.frame("subject_id"=demographics_and_brainstats_imputed$subject_id, "abide_iq_imputed"=demographics_and_brainstats_imputed$iq), file="ABIDE_iq_imputed.csv");
+            # you still have to copy to inst/extdata in the package
+        } else {
+            imputed_file = system.file("extdata", "ABIDE_iq_imputed.csv", package = "brainnet", mustWork = TRUE);
+            abide_imputed = utils::read.table(imputed_file);
+            demographics$iq = abide_imputed$iq;
+
+        }
     } else {
         message("Not imputing data, some subjects will have missing IQ values.");
     }
@@ -81,9 +89,8 @@ load_ABIDE_metadata <- function(impute_data = TRUE, add_merged=TRUE, exclude_bad
     abide_metadata$brainstats = brainstats;
     abide_metadata$demographics = demographics;
     abide_metadata$subjects_list = subjects_list;
-    if(add_merged) {
-        abide_metadata$merged = base::merge(demographics, brainstats, by.x="subject_id", by.y="subject");
-    }
+    abide_metadata$merged = base::merge(demographics, brainstats, by.x="subject_id", by.y="subject");
+
 
     return(abide_metadata);
 }
